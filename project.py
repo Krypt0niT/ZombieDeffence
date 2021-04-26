a8b30cc32288f7562dfbc6abdb03dbbe0f8c1622
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-04-27 01:54:35 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,25 @@
-print(".")
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+window = pygame.display.set_mode((500, 700))
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("ZombieDeffence")
+background = pygame.image.load("hra_template.png")
+
+
+run = True
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+
+    pygame.display.flip()
+
+pygame.quit()
+quit()

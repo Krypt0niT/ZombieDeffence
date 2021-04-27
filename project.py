34e5f59bb5617c9adfd0d449631e12cbdb836d00
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-04-27 10:08:36 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,13 +1,14 @@
-
 import pygame
-from pygame.locals import *
+import random
 
 pygame.init()
-window = pygame.display.set_mode((500, 700))
+gameScreen = pygame.display.set_mode((500, 700))
 clock = pygame.time.Clock()
 
+#terajšie pozadie
+bg = pygame.image.load("hra_template.png")
+
 pygame.display.set_caption("ZombieDeffence")
-background = pygame.image.load("hra_template.png")
 
 
 run = True
@@ -16,7 +17,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    #vytvorenie pozadia pre gameScreen
+    gameScreen.blit(bg, (0, 0))
 
 
     pygame.display.flip()

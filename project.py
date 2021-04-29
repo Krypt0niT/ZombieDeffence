f312cf515e2a885ee1748ba8a0f26766dfe4b627
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-04-29 11:58:47 +0200

    Update project.py

--- project.py
+++ project.py
@@ -24,7 +24,8 @@ rect.center = (w//2, h//2)
 
 VelEnemy = 1
 
-z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11,z12,z13,z14,z15 = False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,
+z1,z2,z3,z4,z5,z6,z7,z8,z9 = False,False,False,False,False,False,False,False,False
+z10,z11,z12,z13,z14,z15 = False,False,False,False,False,False
 
 
 cesta = True

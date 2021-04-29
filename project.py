5135e0e234ae7e61525c689cb2d205a8d8d0eedf
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-04-29 11:48:58 +0200

    Update project.py
    enemy center

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ clock = pygame.time.Clock()
 
 #pohyb enemy
 Rectheight = -100
-Rectwidth = 1392
+Rectwidth = 1367
 
 #terajšie pozadie
 bg = pygame.image.load("hra_template.png")
@@ -20,24 +20,11 @@ pygame.display.set_caption("ZombieDeffence")
 zombie1 = pygame.image.load('zombie_lvl1.png')
 zombie1.convert()
 rect = zombie1.get_rect()
-rect.center = w//2, h//2
+rect.center = (w//2, h//2)
 
 VelEnemy = 1
-z1 = False
-z2 = False
-z3 = False
-z4 = False
-z5 = False
-z6 = False
-z7 = False
-z8 = False
-z9 = False
-z10 = False
-z11 = False
-z12 = False
-z13 = False
-z14 = False
-z15 = False
+
+z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11,z12,z13,z14,z15 = False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,
 
 
 cesta = True
@@ -54,25 +41,25 @@ while run:
     
     if cesta:
         velkosť = 50
-        if Rectwidth == 1392:
+        if Rectwidth == 1367:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight += VelEnemy
             
-        if Rectheight == 430 and Rectwidth == 1392:
+        if Rectheight == 406 and Rectwidth == 1367:
             z4 = True
         if z4:
 
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth -= VelEnemy
             
-        if Rectheight == 430 and Rectwidth == 1177:
+        if Rectheight == 406 and Rectwidth == 1153:
             z5 = True
         if z5:
             z4 = False
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight -= VelEnemy
             
-        if Rectheight == 107 and Rectwidth == 1177:
+        if Rectheight == 83 and Rectwidth == 1153:
             z1 = True  
             z2 = False
             z5 = False
@@ -80,75 +67,75 @@ while run:
 
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth -= VelEnemy
-        if Rectheight == 107 and Rectwidth == 527:
+        if Rectheight == 83 and Rectwidth == 503:
             z2 = True
             z1 = False
 
         if z2:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight += VelEnemy
-        if Rectheight == 431 and Rectwidth == 527:
+        if Rectheight == 407 and Rectwidth == 503:
             z2 = False
             z3 = True
             
         if z3:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth += VelEnemy
-        if Rectheight == 431 and Rectwidth == 743:
+        if Rectheight == 407 and Rectwidth == 719:
             z6 = True
             z3 = False  
         if z6:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight -= VelEnemy
-        if Rectheight == 322 and Rectwidth == 743:
+        if Rectheight == 298 and Rectwidth == 719:
             z7 = True
             z6 = False
         if z7:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth += VelEnemy
-        if Rectheight == 322 and Rectwidth == 851:
+        if Rectheight == 298 and Rectwidth == 827:
             z8 = True
             z7 = False
         if z8:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight += VelEnemy
-        if Rectheight == 647 and Rectwidth == 851:
+        if Rectheight == 623 and Rectwidth == 827:
             z9 = True
             z8 = False
         if z9:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth -= VelEnemy
-        if Rectheight == 647 and Rectwidth == 526:
+        if Rectheight == 623 and Rectwidth == 502:
             z10 = True
             z9 = False
         if z10:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight += VelEnemy
-        if Rectheight == 864 and Rectwidth == 526:
+        if Rectheight == 840 and Rectwidth == 502:
             z11 = True
             z10 = False
         if z11:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth += VelEnemy
-        if Rectheight == 864 and Rectwidth == 1067:
+        if Rectheight == 840 and Rectwidth == 1043:
             z12 = True
             z11 = False
         if z12:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight -= VelEnemy
-        if Rectheight == 647 and Rectwidth == 1067:
+        if Rectheight == 623 and Rectwidth == 1043:
             z13 = True
             z12 = False
         if z13:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectwidth += VelEnemy
-        if Rectheight == 647 and  Rectwidth == 1282:
+        if Rectheight == 623 and  Rectwidth == 1258:
             z14 = True
             z13 = False
         if z14:
             gameScreen.blit(zombie1, (Rectwidth,Rectheight))
             Rectheight += VelEnemy
-        if Rectheight == 970 and Rectwidth == 1282:
+        if Rectheight == 946 and Rectwidth == 1258:
             z15 = True
             z14 = False
         if z15:

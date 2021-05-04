8128c18202968fc1b0ddddefbc02e310493f3c23
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-05-04 19:02:27 +0200

    Update pohyb_lvl1.py

--- pohyb_lvl1.py
+++ pohyb_lvl1.py
@@ -54,217 +54,122 @@ def pohyb_lvl1(Enemy):
             test = 0
             for i in range(Enemy):
                 
-                if E[test][0] == 1367:
+                if E[test][0] == 1367 and (-100 <= E[test][1] <= 406):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] += E[test][3]
                     E[test][5] = 0
                     test += 1
+  
+
             test = 0    
-            for j in range(Enemy):
-                if E[test][1] == 406 and E[test][0] == 1367:
-                    z1 = True
-                    test += 1
-            test = 0    
-            for k in range(Enemy):
-                if z1:
+            for i in range(Enemy):
+                if (1153 <= E[test][0] <= 1367) and E[test][1] == 406:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] -= E[test][3]
                     E[test][5] = -90
                     test += 1
             test = 0
-            for l in range(Enemy):
-                if E[test][1] == 406 and E[test][0] == 1153:
-                    z2 = True
-                    test += 1
-            test = 0
-            for m in range(Enemy):
-                if z2:
-                    z1 = False
+            for i in range(Enemy):
+                if (81 <= E[test][1] <= 406) and E[test][0] == 1153:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] -= E[test][3]
                     E[test][5] = 180
                     test += 1
             test = 0
-            for n in range(Enemy):    
-                if E[test][1] == 83 and E[test][0] == 1153:
-                    z3 = True  
-                    z2 = False
-                    z1 = False
-                    test += 1
-            test = 0
-            for o in range(Enemy):
-                if z3:
-
+            for i in range(Enemy):
+                if E[test][1] == 83 and (503 <= E[test][0] <= 1153):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] -= E[test][3]
                     E[test][5] = -90
                     test += 1
             test = 0
-            for p in range(Enemy):
-                if E[test][1] == 83 and E[test][0] == 503:
-                    z4 = True
-                    z3 = False
-                    test += 1
-            test = 0
-            for p in range(Enemy):
-                if z4:
+            for i in range(Enemy):
+                if (83 <= E[test][1] <= 407) and E[test][0] == 503:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] += E[test][3]
                     E[test][5] = 0
                     test += 1 
             test = 0
             for i in range(Enemy):
-                if E[test][1] == 407 and E[test][0] == 503:
-                    z4 = False
-                    z5 = True
-                    test += 1
-            test = 0
-            for j in range(Enemy):
-                if z5:
+                if E[test][1] == 407 and (503 <= E[test][0] <= 719):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] += E[test][3]
                     E[test][5] = 90
                     test += 1
             test = 0
-            for k in range(Enemy):
-                if E[test][1] == 407 and E[test][0] == 719:
-                    z6 = True
-                    z5 = False
-                    test += 1
-            test = 0  
-            for l in range(Enemy):
-                if z6:
+            for i in range(Enemy):
+                if (298 <= E[test][1] <= 407) and E[test][0] == 719:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] -= E[test][3]
                     E[test][5] = 180
                     test += 1
             test = 0
-            for m in range(Enemy):
-                if E[test][1] == 298 and E[test][0] == 719:
-                    z7 = True
-                    z6 = False
-                    test += 1
-            test = 0
-            for n in range (Enemy):
-                if z7:
+            for i in range(Enemy):
+                if E[test][1] == 298 and (827 >= E[test][0] >= 719):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] += E[test][3]
                     E[test][5] = 90
                     test += 1
             test = 0
-            for o in range(Enemy):
-                if E[test][1] == 298 and E[test][0] == 827:
-                    z8 = True
-                    z7 = False
-                    test += 1
-            test = 0
-            for p in range(Enemy):
-                if z8:
+            for i in range(Enemy):
+                if (298 <= E[test][1] <= 623) and E[test][0] == 827:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] += E[test][3]
                     E[test][5] = 0
                     test += 1
             test = 0
             for i in range(Enemy): 
-                if E[test][1] == 623 and E[test][0] == 827:
-                    z9 = True
-                    z8 = False
-                    test += 1
-            test = 0
-            for j in range(Enemy):
-                if z9:
+                if E[test][1] == 623 and (502 <= E[test][0] <= 827):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] -= E[test][3]
                     E[test][5] = -90
                     test += 1
             test = 0
-            for k in range(Enemy): 
-                if E[test][1] == 623 and E[test][0] == 502:
-                    z10 = True
-                    z9 = False
-                    test += 1
-            test = 0
-            for l in range(Enemy):
-                if z10:
+            for i in range(Enemy): 
+                if (840 >= E[test][1] >= 623) and E[test][0] == 502:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] += E[test][3]
                     E[test][5] = 0
                     test += 1
             test = 0
-            for m in range(Enemy):
-                if E[test][1] == 840 and E[test][0] == 502:
-                    z11 = True
-                    z10 = False
-                    test += 1
-            test = 0
-            for n in range(Enemy):
-                if z11:
+            for i in range(Enemy):
+                if E[test][1] == 840 and (1043 >= E[test][0] >= 502):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] += E[test][3]
                     E[test][5] = 90
                     test += 1
             test = 0
-            for o in range(Enemy):
-                if E[test][1] == 840 and E[test][0] == 1043:
-                    z12 = True
-                    z11 = False
-                    test += 1 
-            test = 0
-            for p in range(Enemy): 
-                if z12:
+            for i in range(Enemy):
+                if (840 >= E[test][1] >= 623) and E[test][0] == 1043:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] -= E[test][3]
                     E[test][5] = 180
                     test += 1
             test = 0
             for i in range(Enemy):
-                if E[test][1] == 623 and E[test][0] == 1043:
-                    z13 = True
-                    z12 = False
-                    test += 1
-            test = 0
-            for j in range(Enemy):
-                if z13:
+                if E[test][1] == 623 and (1258 >= E[test][0] >= 1043):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] += E[test][3]
                     E[test][5] = 90
                     test += 1
             test = 0 
-            for k in range(Enemy):
-                if E[test][1] == 623 and  E[test][0] == 1258:
-                    z14 = True
-                    z13 = False
-                    test += 1
-            test = 0
-            for l in range(Enemy):
-                if z14:
+            for i in range(Enemy):
+                if (946 >= E[test][1] >= 623) and  E[test][0] == 1258:
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][1] += E[test][3]
                     E[test][5] = 0
                     test += 1
             test = 0
-            for m in range(Enemy):
-                if E[test][1] == 946 and E[test][0] == 1258:
-                    z15 = True
-                    z14 = False 
-                    test += 1
-            test = 0
-            for n in range(Enemy):
-                if z15:
+            for i in range(Enemy):
+                if E[test][1] == 946 and (400 <= E[test][0] <= 1258):
                     gameScreen.blit(pygame.transform.rotate(zombie1,E[test][5]),(E[test][0],E[test][1]))
                     E[test][0] -= E[test][3]
                     E[test][5] = -90
                     test += 1
             test = 0
-            for o in range(Enemy):
+            for i in range(Enemy):
                 if E[test][0]  == 400:
-                    z16 = True
-                    test += 1
-            test = 0
-            for p in range(Enemy): 
-                if z16:
                     move = False
-                    z15 = False
                     run = False
                     test += 1
             test = 0

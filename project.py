edea578c28b6afc065441658cb3a1d7b76c6f8ff
Krypt0niT <82445388+Krypt0niT@users.noreply.github.com>
2021-05-23 14:27:22 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,32 @@
+import pygame
+from pohyb_lvl1 import pohyb_lvl1
+import time
+
+pygame.init()
+gameScreen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Zombie Deffence")
+icon = pygame.image.load("images/zombie/zombie_lvl1_1.png")
+pygame.display.set_icon(icon)
+
+#terajšie pozadie
+bg = pygame.image.load("hra_template.png")
+
+Enemy = 20
+kolo = True
+run = True
+
+
+while run:
+    clock.tick(120)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    #vytvorenie pozadia pre gameScreen
+    gameScreen.blit(bg, (0, 0))
+    
+    if kolo:
+        pohyb_lvl1(Enemy)
+pygame.quit()
+quit()
